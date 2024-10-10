8394c89bf73c7356f793d85638a7bb8bdbfbe546
Fabio Setubal <fabiodesenvolvedor7@gmail.com>
2024-10-10 16:40:05 -0300

    Barra de pesquisa finalizada

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from './componentes/Header';
+import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components';
 
 // usando styled para estilizar
@@ -6,12 +7,14 @@ const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
+  overflow: auto;
 `
 
 function App() {
   return (
     <AppContainer>
       <Header/>
+      <Pesquisa/>
     </AppContainer>
   );
 }
