691b2b91659c988bdeaa88d2407a4173aa4bb56b
Fabio Setubal <fabiodesenvolvedor7@gmail.com>
2024-10-10 17:59:27 -0300

    lancamentos finalizado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Header from './componentes/Header';
 import Pesquisa from './componentes/Pesquisa';
 import styled from 'styled-components';
+import UltimosLancamentos from './componentes/UltimosLancamentos';
 
 // usando styled para estilizar
 const AppContainer = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: linear-gradient(90deg, #002f52 35%, #326589);
-  overflow: auto;
 `
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
     <AppContainer>
       <Header/>
       <Pesquisa/>
+      <UltimosLancamentos/>
     </AppContainer>
   );
 }
